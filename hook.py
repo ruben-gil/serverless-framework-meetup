882c889c1ffa05f5c3de76013581732edfd21ec7
Rubén Gil <rubooo@gmail.com>
2019-05-15 11:53:15 +0200

    Add pre and post deployment functions

---
+++ hook.py
@@ -0,0 +1,31 @@
+import json
+import logging
+import boto3
+
+codedeploy = boto3.client('codedeploy')
+
+def pre(event, context):
+
+    deployment_id = event['DeploymentId']
+    lifecycle_event_hook_execution_id = event['LifecycleEventHookExecutionId']
+
+    logging.warning('Check some stuff before traffic has been shifted...')
+
+    return codedeploy.put_lifecycle_event_hook_execution_status(
+        deploymentId = deployment_id,
+        lifecycleEventHookExecutionId = lifecycle_event_hook_execution_id,
+        status = 'Succeeded') 
+        # status can be 'Pending'|'InProgress'|'Succeeded'|'Failed'|'Skipped'|'Unknown'
+
+def post(event, context):
+
+    deployment_id = event['DeploymentId']
+    lifecycle_event_hook_execution_id = event['LifecycleEventHookExecutionId']
+
+    logging.warning('Check some stuff before traffic has been shifted...')
+
+    return codedeploy.put_lifecycle_event_hook_execution_status(
+        deploymentId = deployment_id,
+        lifecycleEventHookExecutionId = lifecycle_event_hook_execution_id,
+        status = 'Succeeded') 
+        # status can be 'Pending'|'InProgress'|'Succeeded'|'Failed'|'Skipped'|'Unknown'
